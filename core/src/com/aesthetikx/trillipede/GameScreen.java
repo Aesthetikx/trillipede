9bfb3bf44d2ab4ec50118da3cfdcb2d9b85d81b4
John DeSilva <desilvjo@umich.edu>
2014-04-18 16:11:35 -0400

    Test texture region

--- core/src/com/aesthetikx/trillipede/GameScreen.java
+++ core/src/com/aesthetikx/trillipede/GameScreen.java
@@ -4,15 +4,20 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 
 public class GameScreen implements Screen {
 
     Trillipede game;
     Texture img;
+    Texture spriteSheet;
+    TextureRegion mushroom;
 
     public GameScreen(final Trillipede game) {
         this.game = game;
         img = new Texture("badlogic.jpg");
+        spriteSheet = new Texture("sheet.png");
+        mushroom = new TextureRegion(spriteSheet, 176, 440, 8, 8);
     }
 
     @Override
@@ -20,7 +25,9 @@ public class GameScreen implements Screen {
         Gdx.gl.glClearColor(1, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         game.batch.begin();
+        game.batch.draw(spriteSheet, 0, 0);
         game.batch.draw(img, 0, 0);
+        game.batch.draw(mushroom, 0, 0);
         game.batch.end();
     }
 
