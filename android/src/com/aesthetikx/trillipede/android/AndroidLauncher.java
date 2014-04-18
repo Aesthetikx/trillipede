73b7df76bd9f84744cb36ee4bb64dc330c2c0c6b
John DeSilva <desilvjo@umich.edu>
2014-04-18 15:39:28 -0400

    Initial commit

---
+++ android/src/com/aesthetikx/trillipede/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.aesthetikx.trillipede.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.aesthetikx.trillipede.Trillipede;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new Trillipede(), config);
+	}
+}
