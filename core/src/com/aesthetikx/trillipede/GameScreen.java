ec2363f22319ee2ac586657a0378d9ca9ab113ac
John DeSilva <desilvjo@umich.edu>
2014-04-19 12:57:06 -0400

    Maintain aspect ratio

--- core/src/com/aesthetikx/trillipede/GameScreen.java
+++ core/src/com/aesthetikx/trillipede/GameScreen.java
@@ -2,10 +2,16 @@ package com.aesthetikx.trillipede;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.Camera;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.utils.Scaling;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -22,8 +28,16 @@ public class GameScreen implements Screen {
 
     List<Mushroom> mushrooms;
 
+    private Camera camera;
+    private Viewport viewport;
+
     public GameScreen(final Trillipede game) {
         this.game = game;
+
+        camera = new OrthographicCamera(256, 256);
+        camera.position.set(128, 128, 0);
+        viewport = new FitViewport(256, 256, camera);
+
         img = new Texture("badlogic.jpg");
         spriteSheet = new Texture("sheet.png");
         mushroomFour = new TextureRegion(spriteSheet, 176, 440, 8, 8);
@@ -36,8 +50,8 @@ public class GameScreen implements Screen {
     private List<Mushroom> createMushrooms() {
         List<Mushroom> mushrooms = new ArrayList<Mushroom>();
         for (int i = 0; i < 100; i++) {
-            int x = MathUtils.random(0, 1080);
-            int y = MathUtils.random(0, 1920);
+            int x = MathUtils.random(0, 256);
+            int y = MathUtils.random(0, 256);
             Mushroom m = new Mushroom(game, this);
             m.setX(x);
             m.setY(y);
@@ -50,6 +64,12 @@ public class GameScreen implements Screen {
     public void render(float delta) {
         Gdx.gl.glClearColor(1, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        camera.update();
+
+        game.batch.setProjectionMatrix(camera.projection);
+        game.batch.setTransformMatrix(camera.view);
+
         game.batch.begin();
         game.batch.draw(spriteSheet, 0, 0);
         game.batch.draw(img, 0, 0);
@@ -61,7 +81,12 @@ public class GameScreen implements Screen {
 
     @Override
     public void resize(int width, int height) {
-
+        Vector2 size = Scaling.fit.apply(256, 256, width, height);
+        int viewportX = (int) (width - size.x) / 2;
+        int viewportY = (int) (height - size.y) / 2;
+        int viewportWidth = (int) size.x;
+        int viewportHeight = (int) size.y;
+        Gdx.gl.glViewport(viewportX, viewportY, viewportWidth, viewportHeight);
     }
 
     @Override
